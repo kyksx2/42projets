605d79fd4d7187673299cbbf80afa9025faf80b3
kjolly <jollykyllian@gmail.com>
2025-09-09 10:04:55 +0200

    cpp09/ex01

--- cpp/cpp09/ex01/main.cpp
+++ cpp/cpp09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kjolly <kjolly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 12:33:12 by kjolly            #+#    #+#             */
-/*   Updated: 2025/09/08 17:18:50 by kjolly           ###   ########.fr       */
+/*   Updated: 2025/09/09 09:42:14 by kjolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,11 @@ bool parsArg(int ac, char *av) {
         if (!goodInput(av[i]))
             return false;
     }
-    true;
+    return true;
+}
+
+bool isOperator(std::string token) {
+    return (token == "+" || token == "-" || token == "/" || token == "*");
 }
 
 int main(int ac, char **av) {
@@ -40,8 +44,33 @@ int main(int ac, char **av) {
     std::string token;
     std::stack<int> s1;
 
+    int a = 0;
+    int b = 0;
     while (iss >> token) {
-        
+        if (isOperator(token)) {
+            if (s1.size() < 2) {
+                std::cerr << "Error: bad operator input";
+                return (1);
+            }
+            else if (s1.size() >= 2) {
+                a = s1.top();
+                s1.pop();
+                b = s1.top();
+                s1.pop();
+                if (token == "+")
+                    s1.push(b + a);
+                else if (token == "-")
+                    s1.push(b - a);
+                else if (token == "*")
+                    s1.push(b * a);
+                else if (token == "/")
+                    s1.push(b / a);
+            }
+        }
+        if (/*s1.size() < 2 && */!isOperator(token)) {
+            s1.push(atoi(token.c_str()));
+        }
     }
+    std::cout << s1.top() << std::endl;
     return (0);
 }
